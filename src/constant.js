1c1ea07a1f086bd9e3e83f45abd19fe8ab018e90
muhammad-junaid1 <mjunaid.swe@gmail.com>
2024-07-02 11:33:47 +0500

    url

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 export const constant = {
 
     //baseUrl: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:5000/" : "https://avenue5-2eacdcb01d69.herokuapp.com/" 
-    baseUrl: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "https://server.idxdubai.com/" : "https://server.idxdubai.com/" 
+    baseUrl: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:5000" : "https://server.idxdubai.com/" 
 }
