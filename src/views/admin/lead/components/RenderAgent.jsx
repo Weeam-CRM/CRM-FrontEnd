f1dd96aae04602b7eeced9d8236b64d644b3a887
muhammad-junaid1 <mjunaid.swe@gmail.com>
2024-08-01 13:43:12 +0500

    renderagent

--- src/views/admin/lead/components/RenderAgent.jsx
+++ src/views/admin/lead/components/RenderAgent.jsx
@@ -1,4 +1,4 @@
-import { Select } from "@chakra-ui/react";
+import { Box, CircularProgress, Select } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
@@ -8,9 +8,10 @@ const RenderAgent = ({ value, managerAssigned, leadID, fetchData }) => {
   const [AgentSelected, setAgentSelected] = useState("");
   const [agents, setAgents] = useState([]);
   const tree = useSelector((state) => state.user.tree);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if(tree && tree["managers"]) {
+    if (tree && tree["managers"]) {
       const agentsList = tree?.agents["manager-" + managerAssigned];
       setAgents(agentsList || []);
       setAgentSelected(value);
@@ -19,27 +20,40 @@ const RenderAgent = ({ value, managerAssigned, leadID, fetchData }) => {
 
   const handleChangeAgent = async (e) => {
     try {
-      await putApi(`api/lead/edit/${leadID}`, {
+      const data = {
         agentAssigned: e.target.value,
-      });
+      };
+
+      await putApi(`api/lead/edit/${leadID}`, data);
       toast.success("Agent updated successfuly");
-      fetchData();
+      setAgentSelected(data.agentAssigned || "");
+      // fetchData();
     } catch (error) {
       console.log(error);
       toast.error("Failed to update the agent");
     }
   };
 
-
   if (agents?.length) {
-    return (
+    return loading ? (
+      <Box
+        border={"1px solid #eee"}
+        borderRadius={"4px"}
+        padding={"3"}
+        display={"flex"}
+        alignItems={"center"}
+      >
+        <p style={{ marginRight: 8 }}>Updating</p>{" "}
+        <CircularProgress size={4} isIndeterminate />
+      </Box>
+    ) : (
       <Select
         placeholder="No Agent"
         onInput={handleChangeAgent}
         value={AgentSelected === null ? "" : AgentSelected}
-          style={{
-        color: !AgentSelected ? "grey" : "black"
-    }}
+        style={{
+          color: !AgentSelected ? "grey" : "black",
+        }}
       >
         {agents?.map((agent) => (
           <option key={agent?._id?.toString()} value={agent?._id?.toString()}>
