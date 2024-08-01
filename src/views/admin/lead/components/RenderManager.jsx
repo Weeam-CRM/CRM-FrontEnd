342b2969d8dad16f0e26951bb10b45eea3b86a5c
muhammad-junaid1 <mjunaid.swe@gmail.com>
2024-08-01 13:41:01 +0500

    renderManager

--- src/views/admin/lead/components/RenderManager.jsx
+++ src/views/admin/lead/components/RenderManager.jsx
@@ -1,4 +1,4 @@
-import { Select } from "@chakra-ui/react";
+import { Box, CircularProgress, Select } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
@@ -7,44 +7,58 @@ import { putApi } from "services/api";
 const RenderManager = ({ value, leadID, fetchData, pageIndex }) => {
   const [ManagerSelected, setManagerSelected] = useState("");
   const tree = useSelector((state) => state.user.tree);
+  const [loading, setLoading] = useState(false);
 
   const handleChangeManager = async (e) => {
     try {
+      setLoading(true);
       const dataObj = {
-        managerAssigned: e.target.value 
-      }
+        managerAssigned: e.target.value,
+      };
 
-      if(e.target.value === "") {
-        dataObj["agentAssigned"] = ""; 
+      if (e.target.value === "") {
+        dataObj["agentAssigned"] = "";
       }
 
       await putApi(`api/lead/edit/${leadID}`, dataObj);
       toast.success("Manager updated successfuly");
-      fetchData(); 
+      setManagerSelected(dataObj.managerAssigned || "");
+      // fetchData();
     } catch (error) {
       console.log(error);
       toast.error("Failed to update the manager");
     }
+    setLoading(false);
   };
 
   useEffect(() => {
+    console.log("effectvalue::", value);
     setManagerSelected(value);
   }, []);
 
-  return (
+  return loading ? (
+    <Box border={"1px solid #eee"} borderRadius={"4px"} padding={"3"} display={"flex"} alignItems={"center"}>
+      <p style={{ marginRight: 8 }}>Updating</p>{" "}
+      <CircularProgress size={4} isIndeterminate />
+    </Box>
+  ) : (
     <Select
-    style={{
-        color: !ManagerSelected ? "grey" : "black"
-    }}
+      style={{
+        color: !ManagerSelected ? "grey" : "black",
+      }}
       value={ManagerSelected || ""}
       onChange={handleChangeManager}
       placeholder="No Manager"
     >
-      {tree && tree?.managers?.map((manager) => (
-        <option key = { manager?._id?.toString()} value={manager?._id?.toString()}>
-          {manager?.firstName + " " + manager?.lastName}
-        </option>
-      ))}
+      {tree &&
+        tree?.managers?.map((manager) => (
+          <option
+            key={manager?._id?.toString()}
+            value={manager?._id?.toString()}
+          >
+            {manager?.firstName + " " + manager?.lastName}
+          </option>
+        ))}
     </Select>
   );
 };
